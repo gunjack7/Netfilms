9a3e7dbd6c8d71eccd35538492cdfe6a4fecb4f4
gunjack7 <131535818+gunjack7@users.noreply.github.com>
2023-12-28 18:47:39 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://openinapp.link/91xkh/`
+					`https://openinapp.link/91xkh`
 				}`,
 			},
 		};
